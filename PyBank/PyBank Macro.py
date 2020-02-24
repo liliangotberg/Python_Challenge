15e9e9ce9424828f67cf1e404818154a993723cb
Liliangotberg@gmail.com <LilianGotberg@gmail.com>
2020-02-24 01:19:08 -0500

    python update

---
+++ PyBank/PyBank Macro.py
@@ -0,0 +1,116 @@
+# First we'll import the os module
+# This will allow us to create file paths across operating systems
+import os
+# Module for reading CSV files
+import cvs
+
+#path to collect data from resource folder
+bankpath_cvs = os.path.join('..', 'Resources', 'budget_data_csv'))
+
+
+# Lists to store data
+# title = []
+# price = []
+# subscribers = []
+# reviews = []
+# review_percent = []
+# length = []
+
+# total_months = "Total Months"
+mylist.count()
+
+# total_sums = "Total Amount"
+# sum(list)
+
+# ave_change = "Average Change"
+# change/total months 
+
+# print("Greatest Increase in Profits:")
+# print("Greatest Decrease in Profits:")
+
+# input of length for you and neighbor
+# months_you_coded = input("How many months have you been coding? ")
+# months_neighbor_coded = input("How many months has your neighbor been coding? ")
+
+# Add total months both have been coding but change int to str in print statement
+# total_months_coded = int(months_you_coded) + int(months_neighbor_coded)
+
+# Print results
+
+print("Together we have been coding for " + str(total_months_coded) + " months!")
+
+print(int(months_you_coded)
+
+
+
+
+# open (bankpath_csv, newline="", encoding='utf-8') as csvfile: 
+
+with open(bankpath_csv, newline="") as csvfile:
+    
+    # CSV reader specifies delimiter and variable that holds contents
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    print(csvreader)
+
+    # Read the header row first (skip this step if there is now header)
+    csv_header = next(csvreader)
+    print(f"CSV Header: {csv_header}")
+
+    # Read each row of data after the header
+    for row in csvreader:
+        print(row)
+
+
+
+
+        
+# Loop through looking for the video
+    for row in csvreader:
+        if row[0] == video:
+            print(row[0] + " is rated " + row[1] + " with a rating of " + row[5])
+
+
+
+
+# Loop through the numbers. (Be sure to cast the string into an integer.)
+    for x in range(start_number, int(user_number) + start_number):
+
+        # Print each number in the range
+        print(x)
+
+
+# Loop through the numbers. (Be sure to cast the string into an integer.)
+    for x in range(user_number):
+
+
+
+
+# Add total month
+total_months = int(months_coded) 
+
+
+total_months = "Total Months"
+total_sums = "Total Amount"
+ave_change = "Average Change" 
+print("Greatest Increase in Profits:")
+print("Greatest Decrease in Profits:")
+
+
+
+
+
+
+
+print("Total Months" + str(age + 1) + " next year.")
+print("The input was converted to " + str(trueOrFalse))
+
+
+# Creates Financial Analysis report.
+print("PyBank Financial Analysis")
+print ($$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$)
+print("Total Months + str(len(total_months))pie_purchases)) + "."))
+print("Total Amount")
+print("Average Change)
+print("Greatest Increase in Profits:")
+print("Greatest Decrease in Profits:")
